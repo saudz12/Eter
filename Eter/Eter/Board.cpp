8a65f69e30198c8f1e4e07d7354f09ee57a1db14
Shaikh Saud <saudshaikh716@gmail.com>
2024-11-01 17:15:44 +0200

    Added Board Class base

---
+++ Eter/Eter/Board.cpp
@@ -0,0 +1,34 @@
+#include "Board.h"
+
+int Board::getPos(int, int)
+{
+    return 0;
+}
+
+void Board::setPos(int, int, int)
+{
+}
+
+void Board::removePos(int, int, int)
+{
+}
+
+char Board::entityWon(int, int, char)
+{
+    return 0;
+}
+
+uint8_t Board::getRowCount()
+{
+    return 0;
+}
+
+uint8_t Board::getColCount()
+{
+    return 0;
+}
+
+uint8_t Board::getMaxSize()
+{
+    return 0;
+}
---
+++ Eter/Eter/Board.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <deque>
+#include <cstdint>
+
+using resizeableMatrix = std::deque<std::deque<std::deque<int>>>;
+using lineChecker = std::deque<std::pair<uint8_t, uint8_t>>; //
+
+class Board
+{
+private:
+	resizeableMatrix m_board;
+	uint8_t m_max_size;
+	lineChecker m_rowChecker;
+	lineChecker m_colChecker;
+	uint8_t m_line_cnt;
+
+public:
+
+	Board() = default;
+
+	int getPos(int, int);
+	void setPos(int, int, int);
+	void removePos(int, int, int);
+	char entityWon(int, int, char);
+
+	uint8_t getRowCount();
+	uint8_t getColCount();
+	uint8_t getMaxSize();
+};
+
