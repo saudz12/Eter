21857f5d35988c4b66b1b97f8da1a2c45fe6016e
FaZ3r <74310446+FaZ3r@users.noreply.github.com>
2024-11-03 11:17:01 +0200

    Added base Game class header and cpp, added a basic constructor for the Player class

---
+++ EterTerminalDistribution/EterTerminalDistribution/Game.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "Player.h"
+#include "Board.h"
+class Game : Player,Board
+{
+	private:
+		Board m_boardGame;
+		Player m_Player1 = { "R" }, m_Player2 = { "B" }; // we don't have a constructor without params in Player class, resorted to this
+		// uint8_t gameType; //could we use this instead of 5 bools?
+		bool m_timed,
+			 m_tournament,
+			 m_training,
+		  	 m_wizardDuel,
+			 m_elementalBattle;
+		uint8_t m_elapsedGames, 
+				m_player1Wins, 
+				m_player2Wins;
+	public:
+		// constructors/destructors
+		Game();
+		//Game(const Game &game);
+		//~Game();
+
+		//getters
+		Board getBoardGame();
+		Player getPlayer1();
+		Player getPlayer2();
+
+		//setters
+		void setBoardGame(Board board);
+
+
+		//methods
+		void timedGame();
+		void tournamentGame();
+		void trainingGame();
+		void wizardDuelGame();
+		void elementalBattleGame();
+
+
+};
+
