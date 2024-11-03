56e9d1bc31fbdf821735db018922545f266e0191
saudz12 <saudshaikh716@gmail.com>
2024-11-03 05:51:32 +0200

    Fixed more things in board class, still needs to be changed to work with minion card.

--- EterTerminalDistribution/EterTerminalDistribution/Board.cpp
+++ EterTerminalDistribution/EterTerminalDistribution/Board.cpp
@@ -1,85 +1,120 @@
 #include "Board.h"
 
+void Board::increaseOnColor(int x, int y, char col)
+{
+	if (col == 'R')
+	{
+		m_rowChecker[x].first++;
+		m_colChecker[y].first++;
+	}
+	else
+	{
+		m_rowChecker[x].second++;
+		m_colChecker[y].second++;
+	}
+}
+
+//REWRITE EVERYTHING DECIDE ON WHAT IS X AND Y
+
 Board::Board()
 {
 	m_board.push_back(line());
 	m_board[0].push_back(cardStack());
 	m_line_cnt = 0;
 	m_max_size = 3;
+	m_rowChecker.emplace_back(0, 0);
+	m_colChecker.emplace_back(0, 0);
 }
 
-int Board::getCardOnPos(int x, int y) {
+int Board::getCardOnPos(int x, int y) {//-1 esec
 	if (x < 0 || y < 0 || x >= m_max_size || y >= m_max_size)
 		return -1;
 	return m_board[x][y].back();
 }
 
-int Board::setPos(int x, int y, int val) { //1 if not succesfull/invalid, 0 if ok
+int Board::setPos(int x, int y, int val, char col) { //1 if not succesfull/invalid, 0 if ok
 	const int boundCondX = XBoundTest(x);
 	const int boundCondY = YBoundTest(y);
 
-	if (boundCondX == 2 || boundCondY == 2)
+	if (boundCondX == OUTSIDE_BOUND || boundCondY == OUTSIDE_BOUND)
 		return -1;
 	
-	if (boundCondX == 0 && boundCondY == 0) {
-		if (val > *m_board[x][y].rbegin()) {
+	if (boundCondX == INSIDE_BOUND && boundCondY == INSIDE_BOUND) {
+		if (posPlaceTest(x, y, val)) {
+			if (getColCount() == 1 && getRowCount() == 1 && m_board[x][y].empty())
+				increaseOnColor(x, y, col);
+			//if(m_board[x][y].back().col != val.col)
+			//if(col == 'R')... scade pt rosu, creste pt albastru
+			//else vice versa <--- de implementat odata cu schimbarea tipului lui val din int in minion card!!!
 			m_board[x][y].push_back(val);
 			return 0;
 		}
-		return -1;
+		else
+			return -1;
 	}
 
-	if (boundCondX == -1) {
-		for (int i = 0; i < getRowCount(); i++) {
-			m_board[i].push_front(std::deque<int>());
+	if (boundCondX != INSIDE_BOUND || boundCondY != INSIDE_BOUND) {
+		if (boundCondX == TOP_BOUND) {
+			x = 0;
+			m_board.push_front(line(getColCount()));
+			m_rowChecker.emplace_front(0, 0);
+			increaseOnColor(x, y, col);
 		}
-	}
 
-	if (boundCondY == -1) {
-		m_board.push_front(std::deque<std::deque<int>>(getColCount()));
-	}
-
-	if (boundCondX == 1) {
-		for (int i = 0; i < getRowCount(); i++) {
-			m_board[i].push_back(std::deque<int>());
+		if (boundCondY == LEFT_BOUND) {
+			y = 0;
+			for (int i = 0; i < getRowCount(); i++) {
+				m_board[i].push_front(cardStack());
+			}
+			m_colChecker.emplace_front(0, 0);
+			increaseOnColor(x, y, col);
 		}
+
+		if (boundCondX == BOTTOM_BOUND) {
+			m_board.push_back(line(getColCount()));	
+			m_rowChecker.emplace_front(0, 0);
+			increaseOnColor(x, y, col);
+		}	
+
+		if (boundCondY == RIGHT_BOUND) {
+			for (int i = 0; i < getRowCount(); i++) {
+				m_board[i].push_back(cardStack());
+			}
+			m_colChecker.emplace_front(0, 0);
+			increaseOnColor(x, y, col);
+		}
+
 	}
 
-	if (boundCondY == 1) {
-		m_board.push_back(std::deque<std::deque<int>>(getColCount()));
-	}
-	
 	m_board[x][y].push_back(val);
+	return 0;
+}
+
+int Board::removePos(int x, int y, int pos) { //1 esec, 0 succes
+	if (XBoundTest(x) != INSIDE_BOUND || YBoundTest(y) != INSIDE_BOUND)
+		return 1;
+	if (pos == 0)
+		m_board[x][y].pop_front();
+	if (pos == m_board[x][y].size()-1)
+		m_board[x][y].pop_back();
+	else
+		m_board[x][y].erase(m_board[x][y].begin()+pos);
 	
 	return 0;
 }
 
-void Board::removePos(int x, int y, int pos) {
-	if (XBoundTest(x) != 0 || YBoundTest(y) != 0)
-		return;
-	m_board[x][y].erase(m_board[x][y].begin()+pos);
-	
-	return;
-}
-
-char Board::entityWon(int x, int y, char col)
+char Board::entityWon(int x, int y, char col) //0 inseamna ca momentan e egal
 {
-
 	if (x < 0 || y < 0 || x >= m_max_size || y >= m_max_size)
 		return 0;
 	if (col == 'R') {
-		if (++m_colChecker[x].first > m_max_size)
-			return 'R';
-		if (++m_rowChecker[y].first > m_max_size)
+		if (++m_rowChecker[x].first == m_max_size || ++m_colChecker[y].first == m_max_size)
 			return 'R';
 	}
 	else {
-		if (++m_colChecker[x].second > m_max_size)
-			return 'B';
-		if (++m_rowChecker[y].second > m_max_size)
+		if (++m_rowChecker[x].second == m_max_size || ++m_colChecker[y].second == m_max_size)
 			return 'B';
 	}
-
 	return 0;
 }
 
@@ -102,32 +137,44 @@ void Board::printBoard()
 {
 	std::cout << "R\\B ";
 	for (int i = 0; i < getColCount(); i++)
-		std::cout << m_colChecker[i].first << "|" << m_colChecker[i].first << " ";
+		std::cout << m_colChecker[i].first << "|" << m_colChecker[i].second << " ";
 	for (int i = 0; i < getRowCount(); i++) {
-		std::cout << "\n" << m_rowChecker[i].first << " " << m_rowChecker[i].second << " ";
-		for (int j = 0; j < getColCount(); i++)
-			std::cout << m_board[i][j].back() << "-" << m_board[i][j].back() << " ";
+		std::cout << "\n" << m_rowChecker[i].first << "|" << m_rowChecker[i].second << " ";
+		for (int j = 0; j < getColCount(); j++)
+			if (!m_board[i][j].empty())
+				std::cout << " " << m_board[i][j].back() << "  ";
+			else
+				std::cout << " -- ";
 	}
 }
 
 int Board::XBoundTest(int x)//0 inside, -1 left margin, 1 right margin, 2 outside
 {
-	if (x >= 0 && x < m_max_size) return 0;
+	if (x >= 0 && x < getRowCount()) return INSIDE_BOUND;
 
-	if (x == -1 && this->getColCount() < m_max_size - 1) return -1;
+	if (x == -1 && getRowCount() < m_max_size) return TOP_BOUND;
 
-	if (x == m_max_size - 1 && this->getColCount() < m_max_size) return 1;
+	if (x == getRowCount() && getRowCount() < m_max_size) return BOTTOM_BOUND;
 
-	return 2;
+	return OUTSIDE_BOUND;
 }
 
 int Board::YBoundTest(int y)//0 inside, -1 top margin, 1 buttom margin, 2 outside
 {
-	if (y >= 0 && y < m_max_size) return 0;
+	if (y >= 0 && y < getColCount()) return INSIDE_BOUND;
 
-	if (y == -1 && this->getRowCount() < m_max_size - 1) return -1;
+	if (y == -1 && getColCount() < m_max_size) return LEFT_BOUND;
 
-	if (y == m_max_size - 1 && this->getRowCount() < m_max_size) return 1;
+	if (y == getColCount() && getColCount() < m_max_size) return RIGHT_BOUND;
 
-	return 2;
+	return OUTSIDE_BOUND;
+}
+
+bool Board::posPlaceTest(int x, int y, int val)
+{
+	if (m_board[x][y].empty())
+		return true;
+	if (val > *m_board[x][y].rbegin())
+		return true;
+	return false;
 }
--- EterTerminalDistribution/EterTerminalDistribution/Board.h
+++ EterTerminalDistribution/EterTerminalDistribution/Board.h
@@ -1,4 +1,12 @@
 #pragma once
+//bound=boundary
+#define TOP_BOUND -1
+#define LEFT_BOUND -1
+#define BOTTOM_BOUND 1
+#define RIGHT_BOUND 1
+#define INSIDE_BOUND 0
+#define OUTSIDE_BOUND 2
+
 #include <deque>
 #include <cstdint>
 #include <iostream>
@@ -6,16 +14,21 @@
 using cardStack = std::deque<int>;
 using line = std::deque<cardStack>;
 using resizeableMatrix = std::deque<line>;
-using lineChecker = std::deque<std::pair<uint8_t, uint8_t>>; //
+using lineChecker = std::deque<std::pair<int, int>>; //<R/B>
 
 class Board
 {
 private:
 	resizeableMatrix m_board;
-	uint8_t m_max_size;
+	uint8_t m_max_size; //in privinta folosirii uint8_t mutam totul mai incolo 
 	lineChecker m_rowChecker;
 	lineChecker m_colChecker;
 	uint8_t m_line_cnt; //for explosions, keep check either here or in game
+
+	void increaseOnColor(int, int, char);
+	int XBoundTest(int);
+	int YBoundTest(int);
+	bool posPlaceTest(int, int, int);
 
 public:
 
@@ -23,8 +36,8 @@ public:
 	~Board() = default;
 	
 	int getCardOnPos(int, int);
-	int setPos(int, int, int);
-	void removePos(int, int, int);
+	int setPos(int, int, int, char);
+	int removePos(int, int, int);
 	char entityWon(int, int, char);
 
 	uint8_t getRowCount();
@@ -32,8 +45,5 @@ public:
 	uint8_t getMaxSize();
 
 	void printBoard();
-	
-	int XBoundTest(int);
-	int YBoundTest(int);
 };
 
