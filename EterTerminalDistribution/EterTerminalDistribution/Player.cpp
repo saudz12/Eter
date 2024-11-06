dab7717376eeadc0b21dd603d5943ca419060bdd
sergiu033 <sergiu.carpinisan.04@gmail.com>
2024-11-06 13:17:46 +0200

    In Played class: -added a pointer for last played minion card with getter and setter -method for placing a minion card from hand -returning a specific minion card to hand -returning last played card to hand

--- EterTerminalDistribution/EterTerminalDistribution/Player.cpp
+++ EterTerminalDistribution/EterTerminalDistribution/Player.cpp
@@ -10,6 +10,11 @@ Player::Player()
 {
 }
 
+Player::~Player()
+{
+	m_lastMinionCardPlayed = nullptr;
+}
+
 char Player::GetPlayerColor() const
 {
 	return m_playerColor;
@@ -23,6 +28,11 @@ bool Player::GetUsedIllusion() const
 const std::unordered_map<MinionCard, uint16_t>& Player::GetHandCards() const
 {
 	return m_handCards;
+}
+
+MinionCard* Player::GetLastMinionCardPlayed() const
+{
+	return m_lastMinionCardPlayed;
 }
 
 void Player::SetPlayerColor(char playerColor)
@@ -40,15 +50,46 @@ void Player::SetHandCards(const std::unordered_map<MinionCard, uint16_t>& handCa
 	m_handCards = handCards;
 }
 
+void Player::SetLastMinionCardPlayed(MinionCard* cardPlayed)
+{
+	m_lastMinionCardPlayed = cardPlayed;
+}
+
 void Player::generateTrainingModeHand()
 {
+	//creating minion cards with their value and color
 	MinionCard combatCard1(1, m_playerColor);
 	MinionCard combatCard2(2, m_playerColor);
 	MinionCard combatCard3(3, m_playerColor);
 	MinionCard combatCard4(4, m_playerColor);
 
-	m_handCards[combatCard1] = 2;
+	//add the minions card in hand with their respective amount
+	m_handCards[combatCard1] = 2; //2 of the combatCard1
 	m_handCards[combatCard2] = 2;
 	m_handCards[combatCard3] = 2;
 	m_handCards[combatCard4] = 1;
+}
+
+bool Player::placeMinionCardFromHand(MinionCard& card)
+{
+	if (m_handCards.at(card) != 0)
+	{
+		m_handCards[card]--;
+		m_lastMinionCardPlayed = &card;
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+void Player::returnMinionCardToHand(const MinionCard& card)
+{
+	m_handCards[card]++;
+}
+
+void Player::returnLastMinionCardToHand()
+{
+	m_handCards[(*m_lastMinionCardPlayed)]++;
 }
--- EterTerminalDistribution/EterTerminalDistribution/Player.h
+++ EterTerminalDistribution/EterTerminalDistribution/Player.h
@@ -11,19 +11,26 @@ private:
 	char m_playerColor;
 	std::unordered_map<MinionCard, uint16_t> m_handCards;
 	bool m_illusionUsage; //true if illusion has been used, false otherwise
+	MinionCard* m_lastMinionCardPlayed; //pointer towards the last card played
 public:
 	//constructor
 	Player(char playerColor);
 	Player();
+	~Player();
 	//getters
 	char GetPlayerColor() const;
 	bool GetUsedIllusion() const;
 	const std::unordered_map<MinionCard, uint16_t>& GetHandCards() const;
+	MinionCard* GetLastMinionCardPlayed() const;
 
 	//setters
 	void SetPlayerColor(char playerColor);
 	void SetIllusionUsage(bool illusionUsage);
 	void SetHandCards(const std::unordered_map<MinionCard, uint16_t>& handCards);
+	void SetLastMinionCardPlayed(MinionCard* cardPlayed);
 
 	void generateTrainingModeHand();
+	bool placeMinionCardFromHand(MinionCard& card); //returns true if card was found in handCards, false otherwise
+	void returnMinionCardToHand(const MinionCard& card); //returns a specific minion card to hand
+	void returnLastMinionCardToHand(); //returns last played minion card to hand
 };
