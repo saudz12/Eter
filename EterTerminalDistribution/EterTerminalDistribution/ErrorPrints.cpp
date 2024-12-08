cdfb24aef550ce3e7a332ce0efe6094ece994a01
FaZ3r <74310446+FaZ3r@users.noreply.github.com>
2024-12-08 17:02:39 +0200

    Adapted ErrorPrints to use the CommonErrors enum class.

--- EterTerminalDistribution/EterTerminalDistribution/ErrorPrints.cpp
+++ EterTerminalDistribution/EterTerminalDistribution/ErrorPrints.cpp
@@ -1,22 +1,22 @@
 #include "ErrorPrints.h"
 
-void ErrorMesageHuricane(int message)
+void ErrorMesageHuricane(CommonErrors message)
 {
 	switch (message)
 	{
-	case OUTSIDE_BOUNDS:
+	case CommonErrors::OUTSIDE_BOUNDS:
 		std::cout << "ERR: Invalid Line.. Chosen line is out of bounds\n";
 		return;
-	case INVALID_LINE_TYPE:
+	case CommonErrors::INVALID_LINE_TYPE:
 		std::cout << "ERR: Invalid Line Type.. Only accepting C for columns and R for rows..\n";
 		return;
-	case INVALID_LINE_STRUCTURE:
+	case CommonErrors::INCOMPLETE_LINE_STRUCTURE:
 		std::cout << "ERR: Invalid Line Structure.. Selected Line has empty spaces..\n";
 		return;
-	case INVALID_DIRECTION:
+	case CommonErrors::INVALID_DIRECTION:
 		std::cout << "ERR: Invalid Direction.. Only accepting U and D for columns and L and R for rows..\n";
 		return;
-	case ETER_PROPERTY_VIOALTION:
+	case CommonErrors::ETER_PROPERTY_VIOLATION:
 		std::cout << "ERR: Invalid Hurricane Usage.. Can't use hurricane on eter card..\n";
 		return;
 	default:
@@ -24,28 +24,28 @@ void ErrorMesageHuricane(int message)
 	}
 }
 
-void ErrorMessageWhirlpool(int message) {
+void ErrorMessageWhirlpool(CommonErrors message) {
 	switch (message)
 	{
-	case INVALID_LINE_TYPE:
+	case CommonErrors::INVALID_LINE_TYPE:
 		std::cout << "ERR: Invalid Line Type.. Only accepting C for columns and R for rows..\n";
 		return;
-	case INVALID_DIRECTION:
+	case CommonErrors::INVALID_DIRECTION:
 		std::cout << "ERR: Invalid Direction.. Only accepting U and D for columns and L and R for rows..\n";
 		return;
-	case EMPTY_SPACE:
+	case CommonErrors::EMPTY_STACK:
 		std::cout << "ERR: Invalid space.. Chosen space must be empty..\n";
 		return;
-	case OUTSIDE_BOUNDS:
+	case CommonErrors::OUTSIDE_BOUNDS:
 		std::cout << "ERR: Invalid Space.. Chosen space is out of bounds..\n";
 		return;
-	case ADJACENT_OUTSIDE_BOUNDS:
+	case CommonErrors::ADJACENT_OUTSIDE_BOUNDS:
 		std::cout << "ERR: Invalid Adjacent.. Adjacent spaces are out of bounds..\n";
 		return;
-	case ADJACENT_SPACES_EMPTY:
+	case CommonErrors::ADJACENT_SPACES_EMPTY:
 		std::cout << "ERR: Invalid Adjacent.. One or both of the adjacents spaces are empty..\n";
 		return;
-	case ETER_PROPERTY_VIOALTION:
+	case CommonErrors::ETER_PROPERTY_VIOLATION:
 		std::cout << "ERR: Invalid Whirlpool Usage.. Can't use hurricane on eter card..\n";
 		return;
 	default:
@@ -53,24 +53,21 @@ void ErrorMessageWhirlpool(int message) {
 	}
 }
 
-void ErrorMessageFlame(int message)
+void ErrorMessageFlame(CommonErrors message)
 {
 	switch (message) {
-	case OUTSIDE_BOUNDS:
+	case CommonErrors::OUTSIDE_BOUNDS:
 		std::cout << "ERR: Invalid slot.. Slot coordinates are out of bounds...\n";
 		return;
-	case FAILED_FLAME_CARD_PLACEMENT:
-		std::cout << "ERR: Failed to place minion card with Flame elemental card\n";
-		return;
 	default:
 		break;
 	}
 }
 
-void ErrorMessageFire(int message)
+void ErrorMessageFire(CommonErrors message)
 {
 	switch (message) {
-	case INVALID_CARD_VALUE:
+	case CommonErrors::INVALID_CARD_VALUE:
 		std::cout << "ERR: Chosen value is invalid... Pick a card between 1 and 4..\n";
 		return;
 	default:
@@ -78,10 +75,10 @@ void ErrorMessageFire(int message)
 	}
 }
 
-void ErrorMessageAsh(int message)
+void ErrorMessageAsh(CommonErrors message)
 {
 	switch (message) {
-	case INVALID_CARD_VALUE:
+	case CommonErrors::INVALID_CARD_VALUE:
 		std::cout << "ERR: Chosen value is invalid... Pick a card between 1 and 4..\n";
 		return;
 	default:
@@ -89,13 +86,13 @@ void ErrorMessageAsh(int message)
 	}
 }
 
-void ErrorMessageSpark(int message)
+void ErrorMessageSpark(CommonErrors message)
 {
 	switch (message) {
-	case OUTSIDE_BOUNDS:
+	case CommonErrors::OUTSIDE_BOUNDS:
 		std::cout << "ERR: Chosen coordinates out of bounds...\n";
 		return;
-	case ETER_PROPERTY_VIOALTION:
+	case CommonErrors::ETER_PROPERTY_VIOLATION:
 		std::cout << "ERR: Can't use Spark elemental card on eter card\n";
 		return;
 	default:
@@ -103,22 +100,22 @@ void ErrorMessageSpark(int message)
 	}
 }
 
-void ErrorMessageSquall(int message)
+void ErrorMessageSquall(CommonErrors message)
 {
 	switch (message) {
-	case OUTSIDE_BOUNDS:
+	case CommonErrors::OUTSIDE_BOUNDS:
 		std::cout << "ERR: Chosen coordinates out of bounds...\n";
 		return;
-	case ETER_PROPERTY_VIOALTION:
+	case CommonErrors::ETER_PROPERTY_VIOLATION:
 		std::cout << "ERR: Can't use Squall elemental card on eter card..\n";
 		return;
-	case EMPTY_SPACE:
+	case CommonErrors::EMPTY_STACK:
 		std::cout << "ERR: Cannot place on empty space..\n";
 		return;
-	case FAILED_ON_ILLUSION:
+	case CommonErrors::ILLUSION_PROPERTY_VIOLATION:
 		std::cout << "ERR: Can't use Squall elemental card on illusion..\n";
 		return;
-	case NOT_ENEMY_CARD:
+	case CommonErrors::NOT_ENEMY_CARD:
 		std::cout << "ERR: Not an opponent card..\n";
 		return;
 	default:
@@ -126,19 +123,19 @@ void ErrorMessageSquall(int message)
 	}
 }
 
-void ErrorMessageGust(int message)
+void ErrorMessageGust(CommonErrors message)
 {
 	switch (message) {
-	case OUTSIDE_BOUNDS:
+	case CommonErrors::OUTSIDE_BOUNDS:
 		std::cout << "ERR: Chosen coordinates out of bounds..\n";
 		return;
-	case ETER_PROPERTY_VIOALTION:
+	case CommonErrors::ETER_PROPERTY_VIOLATION:
 		std::cout << "ERR: Can't use Gust elemental card on eter card..\n";
 		return;
-	case IDENTICAL_COORDINATES:
+	case CommonErrors::IDENTICAL_COORDINATES:
 		std::cout << "ERR: Chosen coordinates are identical to your current coordinates...\n";
 		return;
-	case VALUE_TOO_GREAT:
+	case CommonErrors::INVALID_CARD_VALUE:
 		std::cout << "ERR: Can't place on top of card of greater value...\n";
 		return;
 	default:
@@ -146,13 +143,13 @@ void ErrorMessageGust(int message)
 	}
 }
 
-void ErrorMessageMirage(int message)
+void ErrorMessageMirage(CommonErrors message)
 {
 	switch (message) {
-	case OUTSIDE_BOUNDS:
+	case CommonErrors::OUTSIDE_BOUNDS:
 		std::cout << "ERR: Chosen coordinates out of bounds..\n";
 		return;
-	case NOT_ILLUSION:
+	case CommonErrors::NOT_ILLUSION:
 		std::cout << "ERR: Chosen card position is not an illusion..\n";
 		return;
 	default:
@@ -160,13 +157,13 @@ void ErrorMessageMirage(int message)
 	}
 }
 
-void ErrorMessagestorm(int message)
+void ErrorMessagestorm(CommonErrors message)
 {
 	switch (message) {
-	case OUTSIDE_BOUNDS:
+	case CommonErrors::OUTSIDE_BOUNDS:
 		std::cout << "ERR: Chosen coordinates out of bounds..\n";
 		return;
-	case INSUFFICIENT_STACK_HEIGHT:
+	case CommonErrors::STACK_HEIGHT_TOO_SMALL:
 		std::cout << "ERR: The stack does not have enough cards..\n";
 		return;
 	default:
@@ -180,7 +177,7 @@ void ErrorMessageTide(CommonErrors message)
 	case CommonErrors::OUTSIDE_BOUNDS:
 		std::cout << "ERR: Chosen coordinates out of bounds..\n";
 		return;
-	case CommonErrors::ETER_PROPERTY_VIOALTION:
+	case CommonErrors::ETER_PROPERTY_VIOLATION:
 		std::cout << "ERR: Can't use Tide elemental card on eter card..\n";
 		return;
 	default:
@@ -199,9 +196,9 @@ void ErrorMessageMist(CommonErrors message)
 	}
 }
 
-void ErrorMessageWave(int message) {
+void ErrorMessageWave(CommonErrors message) {
 	switch (message) {
-	case ETER_PROPERTY_VIOALTION:
+	case CommonErrors::ETER_PROPERTY_VIOLATION:
 		std::cout << "ERR: Can't use Wave elemental power on eter card\n";
 		return;
 	default:
--- EterTerminalDistribution/EterTerminalDistribution/ErrorPrints.h
+++ EterTerminalDistribution/EterTerminalDistribution/ErrorPrints.h
@@ -2,44 +2,6 @@
 #include <cstdint>
 #include <iostream>
 
-//Shared
-constexpr int16_t NO_ERRORS = 0;
-constexpr int16_t OUTSIDE_BOUNDS = -1;
-constexpr int16_t ETER_PROPERTY_VIOALTION = -2;
-constexpr int16_t EMPTY_SPACE = -3;
-constexpr int16_t INVALID_LINE_TYPE = -4;
-constexpr int16_t INVALID_DIRECTION = -5;
-
-
-//Hurricane span
-constexpr int16_t INVALID_LINE_STRUCTURE = 2;
-
-//Whirlpool
-constexpr int16_t ADJACENT_OUTSIDE_BOUNDS = 1;
-constexpr int16_t ADJACENT_SPACES_EMPTY = 2;
-
-//Flame
-constexpr int16_t FAILED_FLAME_CARD_PLACEMENT = 1;
-
-//Fire/Ash
-constexpr int16_t INVALID_CARD_VALUE = 1;
-
-//Squall
-constexpr int16_t FAILED_ON_ILLUSION = 3;
-constexpr int16_t NOT_ENEMY_CARD = 1;
-
-//Gust
-constexpr int16_t IDENTICAL_COORDINATES = 1;
-constexpr int16_t VALUE_TOO_GREAT = 2;
-
-//Mirage
-constexpr int16_t NOT_ILLUSION = 1;
-
-//Storm
-constexpr int16_t INSUFFICIENT_STACK_HEIGHT = 1;
-
-//Mist
-constexpr int16_t FAILED_MIST_CARD_PLACEMENT = 1;
 
 enum class CommonErrors : int16_t {
 	NO_ERRORS,
@@ -68,16 +30,16 @@ enum class CommonErrors : int16_t {
 
 };
 
-void ErrorMesageHuricane(int message);
-void ErrorMessageWhirlpool(int message);
-void ErrorMessageFlame(int message);
-void ErrorMessageFire(int message);
-void ErrorMessageAsh(int message);
-void ErrorMessageSpark(int message);
-void ErrorMessageSquall(int message);
-void ErrorMessageGust(int message);
-void ErrorMessageMirage(int message);
-void ErrorMessagestorm(int message);
-void ErrorMessageTide(int message);
-void ErrorMessageMist(int message);
-void ErrorMessageWave(int message);
+void ErrorMesageHuricane(CommonErrors message);
+void ErrorMessageWhirlpool(CommonErrors message);
+void ErrorMessageFlame(CommonErrors message);
+void ErrorMessageFire(CommonErrors message);
+void ErrorMessageAsh(CommonErrors message);
+void ErrorMessageSpark(CommonErrors message);
+void ErrorMessageSquall(CommonErrors message);
+void ErrorMessageGust(CommonErrors message);
+void ErrorMessageMirage(CommonErrors message);
+void ErrorMessagestorm(CommonErrors message);
+void ErrorMessageTide(CommonErrors message);
+void ErrorMessageMist(CommonErrors message);
+void ErrorMessageWave(CommonErrors message);
